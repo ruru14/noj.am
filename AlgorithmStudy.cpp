a21c6405d7dee7eae5f4a78720f8b5b1b46e006a
JaeWon Lee <ljw2889@gmail.com>
2021-08-03 20:25:05 +0900

    [Solve] 1012 11399

--- AlgorithmStudy.cpp
+++ AlgorithmStudy.cpp
@@ -1,7 +1,7 @@
 ﻿// AlgorithmStudy.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 //#include <bits/stdc++.h>
-#include "Solve/17390.cpp"
+#include "Solve/1012.cpp"
 using namespace std;
 //using namespace nojam2630;
 
@@ -14,7 +14,7 @@ int main()
 	std::cin.tie(NULL);
 	std::ios::sync_with_stdio(false);
 	*/
-	nojam17390::solution();
+	nojam1012::solution();
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
