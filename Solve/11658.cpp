21ca53a928127f5666f4d739b57c624e38887020
JaeWon Lee <ljw2889@gmail.com>
2021-08-06 14:01:31 +0900

    [Solve] 11658 2042

--- Solve/11658.cpp
+++ Solve/11658.cpp
@@ -1,50 +1,58 @@
 #include "11658.h"
 
-int mem[1025][1025] = { {0,} };
-int ori[1025][1025] = { {0,} };
-int trs[1025][1025] = { {0,} };
+int n, m;
+int arr[1025][1025] = { {0,} };
+int tree[1025][1025] = { {0,} };
+
+int sum(int index_x, int index_y) {
+	int ans = 0;
+	while (index_x > 0) {
+
+		int temp_y = index_y;
+		while (temp_y > 0) {
+			ans += tree[index_x][temp_y];
+			temp_y -= (temp_y & -temp_y);
+		}
+		index_x -= (index_x & -index_x);
+	}
+	return ans;
+}
+
+void update(int index_x, int index_y, int value) {
+	while (index_x <= n) {
+		int temp_y = index_y;
+		while (temp_y <= n) {
+			tree[index_x][temp_y] += value;
+			temp_y += (temp_y & -temp_y);
+		}
+		index_x += (index_x & -index_x);
+	}
+}
 
 void nojam11658::solution()
 {
-	int n, m;
-	int w, x, y, z, v, c;
 	scanf("%d %d", &n, &m);
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			int tmp;
-			scanf("%d", &tmp);
-			mem[i + 1][j + 1] = mem[i + 1][j] + mem[i][j + 1] - mem[i][j] + tmp;
-			ori[i][j] = tmp;
+	for (int i = 1; i < n + 1; i++) {
+		for (int j = 1; j < n + 1; j++) {
+			scanf("%d", &arr[i][j]);
+			update(i, j, arr[i][j]);
 		}
 	}
 
-	for (int i = 0; i < m; i++) {
-		scanf("%d", &w);
-		if (w == 0) {
-			scanf("%d %d %d", &x, &y, &c);
-			x--;
-			y--;
-			trs[x][y] = c - ori[x][y];
-			//printf("B : %d %d : %d\n", ori[x][y], c, trs[x][y]);
-			ori[x][y] = c;
-			/*printf("S : %d %d\n", x, y);
-			for (int a = 0; a < n; a++) {
-				for (int b = 0; b < n; b++) {
-					printf("%d ", trs[a][b]);
-				}
-				printf("\n");
-			}*/
+	while (m--) {
+		int flag;
+		scanf("%d", &flag);
+		if (flag == 0) {
+			int x, y, value;
+			scanf("%d %d %d", &x, &y, &value);
+			update(x, y, value - arr[x][y]);
+			arr[x][y] = value;
 		}
-		else {
-			scanf("%d %d %d %d", &x, &y, &z, &v);
-			int ans = mem[z][v] + mem[x - 1][y - 1] - mem[x - 1][v] - mem[z][y - 1];
-			for (int a = x - 1; a <= z; a++) {
-				for (int b = y - 1; b <= v; b++) {
-					ans += trs[a][b];
-				}
-			}
-			printf("%d\n", ans);
+		else if (flag == 1) {
+			int x1, y1, x2, y2;
+			scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+			printf("%d\n", sum(x2, y2) + sum(x1 - 1, y1 - 1) - sum(x2, y1 - 1) - sum(x1 - 1, y2));
 		}
 	}
 }
