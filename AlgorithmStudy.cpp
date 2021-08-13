2e961695f7c4fec27e68363b5470d2529d5c75a9
JaeWon Lee <ljw2889@gmail.com>
2021-08-13 15:11:18 +0900

    [Solve] 1541

--- AlgorithmStudy.cpp
+++ AlgorithmStudy.cpp
@@ -1,21 +1,19 @@
 ﻿// AlgorithmStudy.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 //#include <bits/stdc++.h>
-#include "Solve/2309.cpp"
+#include "Solve/1541.cpp"
 using namespace std;
 //using namespace nojam2630;
 
 int main()
 {
-
-	//printf("%d", 0 != 1);
-	//printf("%d", c);
-	//printf("%s", &a);
+	printf("%d", abs(-1));
 	/*
 	std::cin.tie(NULL);
 	std::ios::sync_with_stdio(false);
 	*/
-	nojam2309::solution();
+
+	//nojam1541::solution();
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
---
+++ Solve/1541.h
@@ -0,0 +1,8 @@
+#ifndef nojam1541
+#define nojam1541
+#include "../AlgorithmStudy.h"
+namespace nojam1541 {
+	void solution();
+}
+
+#endif
