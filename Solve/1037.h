6b489c5a2fb02ef3467d8002e9c686292a26b605
JaeWon Lee <ljw2889@gmail.com>
2021-09-05 14:36:48 +0900

    [Solve] 1037

---
+++ Solve/1037.h
@@ -0,0 +1,8 @@
+#ifndef nojam1037
+#define nojam1037
+#include "../AlgorithmStudy.h"
+namespace nojam1037 {
+	void solution();
+}
+
+#endif
---
+++ Solve/5086.h
@@ -0,0 +1,8 @@
+#ifndef nojam5086
+#define nojam5086
+#include "../AlgorithmStudy.h"
+namespace nojam5086 {
+	void solution();
+}
+
+#endif
