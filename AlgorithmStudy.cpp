ece31ac1e0b9bb27e0957e92561137663123f153
JaeWon Lee <ljw2889@gmail.com>
2021-09-07 13:05:11 +0900

    [Solve] 6588

--- AlgorithmStudy.cpp
+++ AlgorithmStudy.cpp
@@ -1,7 +1,7 @@
 ﻿// AlgorithmStudy.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 //#include <bits/stdc++.h>
-#include "Solve/1037.cpp"
+#include "Solve/6588.cpp"
 #include "Libs.cpp"
 using namespace std;
 //using namespace nojam2630;
@@ -9,6 +9,7 @@ using namespace std;
 
 int main()
 {
+
 	//printf("%c", 47);
 	//printf("%s", Libs::minus_temp("10", "1").c_str());
 	//plus_bignum("123", "123");
@@ -17,7 +18,7 @@ int main()
 	std::ios::sync_with_stdio(false);
 	*/
 
-	nojam1037::solution();
+	nojam6588::solution();
 	//Libs::solution();
 }
 
--- Solve/18352.cpp
+++ Solve/18352.cpp
@@ -1,8 +1,48 @@
 #include "18352.h"
 
+int n, m, k, x;
+int length[300001];
+std::vector<std::vector<int>> adjList(300001);
+
+void adjacencyBFS(int start, int dest) {
+	std::queue<int> queue;
+	length[start] = 0;
+	int ans = 0;
+
+	queue.push(start);
+	while (!queue.empty()) {
+		int current = queue.front();
+		queue.pop();
+		for (int i = 0; i < adjList.at(current).size(); i++) {
+			if (length[adjList.at(current)[i]] == -1) {
+				length[adjList.at(current)[i]] = length[current] + 1;
+				queue.push(adjList.at(current)[i]);
+			}
+		}
+
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (length[i] == dest) {
+			printf("%d\n", i);
+			ans++;
+		}
+	}
+	if (ans == 0) {
+		printf("-1");
+	}
+}
+
 void nojam18352::solution()
 {
-	int n, m, k, x;
 	scanf("%d %d %d %d", &n, &m, &k, &x);
+	std::fill_n(length, 300000, -1);
 
+	while (m--) {
+		int from, to;
+		scanf("%d %d", &from, &to);
+		adjList[from].push_back(to);
+	}
+
+	adjacencyBFS(x, k);
 }
