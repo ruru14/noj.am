772dae622d9b77c40781ace53a7a746c5e500cdf
Sol Lee <ljw2889@gmail.com>
2025-05-13 11:17:59 +0900

    [9466] [G3] 텀 프로젝트 : DFS, 사이클 탐지

---
+++ boj.kr/noj.am.9466/noj.am.9466.cpp
@@ -0,0 +1,62 @@
+// noj.am.9466.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+void dfs(int cur, vector<int>& graph, vector<bool>& cycle, vector<int>& state) {
+    state[cur] = 1;
+    int next = graph[cur];
+
+    if (state[next] == 0) dfs(next, graph, cycle, state);
+    else if (state[next] == 1) {
+        while (!cycle[next]) {
+            cycle[next] = true;
+            next = graph[next];
+        }
+    }
+
+    state[cur] = 2;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int tc, n;
+    cin >> tc;
+
+    while (tc--) {
+        int ans = 0;
+        cin >> n;
+        vector<int> graph(n + 1, 0);
+        vector<bool> cycle(n + 1, false);
+        vector<int> state(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            cin >> graph[i];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (state[i] == 0) dfs(i, graph, cycle, state);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!cycle[i]) ans++;
+        }
+
+        cout << ans << "\n";
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
