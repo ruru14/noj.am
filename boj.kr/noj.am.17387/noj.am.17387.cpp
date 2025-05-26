25197f73423a6c996df00cb68f3726d728668d0c
Sol Lee <ljw2889@gmail.com>
2025-05-26 14:07:39 +0900

    [17387] [G2] 선분 교차 2 : 기하학, 선분 교차 판정, CCW

---
+++ boj.kr/noj.am.17387/noj.am.17387.cpp
@@ -0,0 +1,40 @@
+// noj.am.17387.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+int ccw(pair<long long, long long> a, pair<long long, long long> b, pair<long long, long long> c) {
+	long long flag = (b.first - a.first) * (c.second - a.second) - (b.second - a.second) * (c.first - a.first);
+	if (flag == 0) return 0;
+	else return flag > 0 ? 1 : -1;
+}
+
+int main() {
+	pair<long long, long long> a1, a2, b1, b2;
+	cin >> a1.first >> a1.second >> a2.first >> a2.second;
+	cin >> b1.first >> b1.second >> b2.first >> b2.second;
+	int ab1 = ccw(a1, a2, b1) * ccw(a1, a2, b2);
+	int ab2 = ccw(b1, b2, a1) * ccw(b1, b2, a2);
+	if (ab1 <= 0 && ab2 <= 0) {
+		if (ab1 == 0 && ab2 == 0) {
+			if (a1 > a2) swap(a1, a2);
+			if (b1 > b2) swap(b1, b2);
+			cout << (a1 <= b2 && b1 <= a2 ? "1" : "0");
+		} else cout << "1";
+	}else cout << "0";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
